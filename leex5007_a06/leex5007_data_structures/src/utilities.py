0972482ad7e43b10f7b50e76c49eab956550d4a2
Kaiden Lee <110312105+kaidenslee@users.noreply.github.com>
2025-04-27 18:40:58 -0400

    Update utilities.py

--- leex5007_a06/leex5007_data_structures/src/utilities.py
+++ leex5007_a06/leex5007_data_structures/src/utilities.py
@@ -2,9 +2,8 @@
 -------------------------------------------------------
 Utilities Functions
 -------------------------------------------------------
-Author:  David Brown
-ID:      123456789
-Email:   dbrown@wlu.ca
+This file was originally provided as a template. Not my 
+personal work.
 __updated__ = "2024-01-30"
 -------------------------------------------------------
 """
